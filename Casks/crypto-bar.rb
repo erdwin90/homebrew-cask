3583e8ef5a1a361ecf16d0c50e600bbcccc22ed8
Edwin <aimeenader48@gmail.com>
2026-07-04 08:31:58 -0500

    Update crypto-bar.rb

--- Casks/crypto-bar.rb
+++ Casks/crypto-bar.rb
@@ -1,5 +1,5 @@
 cask "crypto-bar" do
-  version "v1.2.0"
+  version "1.2.0"
   sha256 "58ea26909cfeda96c357c5211630e31e21c74fe448af80c8ef0db4e7e5a4629e"
 
   url "https://github.com/erdwin90/crypto-bar-mac/releases/download/v#{version}/CryptoBar.dmg"
