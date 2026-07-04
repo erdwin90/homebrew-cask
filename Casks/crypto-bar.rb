0f28e553d2c445dd34db2ba9293035a74edcb4ae
Edwin <aimeenader48@gmail.com>
2026-07-04 08:25:14 -0500

    Add files via upload

---
+++ Casks/crypto-bar.rb
@@ -0,0 +1,12 @@
+cask "crypto-bar" do
+  version "1.1.16"
+  sha256 "58ea26909cfeda96c357c5211630e31e21c74fe448af80c8ef0db4e7e5a4629e"
+
+  url "https://github.com/erdwin90/crypto-bar-mac/releases/download/v#{version}/CryptoBar.dmg"
+
+  name "Crypto Bar"
+  desc "Cryptocurrency ticker for the macOS menu bar"
+  homepage "https://github.com/erdwin90/crypto-bar-mac"
+
+  app "CryptoBar.app"
+end
